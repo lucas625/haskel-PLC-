2af088ec353c22b2fd65c2504070b36fc8ccd533
Lucas Aurelio Gomes Costa <lagc@WINDOWS.CIN.UFPE.BR>
2019-03-07 09:45:14 -0300

    finished aula 05

--- Março/aula_05.hs
+++ Março/aula_05.hs
@@ -11,3 +11,46 @@ digitosLista (a:as)
 ehDigito :: Char -> Bool
 ehDigito ch = ('0' <= ch) && (ch <= '9')
 
+
+--encontra o maior valor de uma lista de inteiros
+--maiorLista :: [Int] -> Int
+--maiorLista [] = minBound :: Int
+--maiorLista [x] = x
+--maiorLista (x:xs) =
+-- | x > maiorLista xs = x
+-- | otherwise = maiorLista xs
+-- perceba que apenas construtores podem ser usados para gerar padrões (:)
+
+--retorna o primeiro digito de uma lista
+primeiroDigito :: String -> Char
+primeiroDigito st = case (digitosLista st) of
+    [] -> '\0'
+    (x:xs) -> x
+
+-- Polimorfismo (função usada para tipos diferentes)
+
+--serve para lista de quaisquer tipos (polimorfismo parametrico)
+tamLista :: [t] -> Int
+tamLista [] = 0
+tamLista (x:xs) = 1 + tamLista xs
+
+--zip :: [t] -> [u] -> [(t,u)]
+--falta continuar
+
+ehPar :: Int -> Bool
+ehPar x = (mod x 2) == 0
+
+dobrarLista :: [Int] -> [Int]
+dobrarLista l = [ 2*x | x <- l]
+
+dobrarListaPar :: [Int] -> [Int]
+dobrarListaPar l = [2*x | x <- l, ehPar x]
+
+somarPares :: [(Int, Int)] -> [Int]
+somarPares l = [ x + y | (x,y) <- l]
+
+l1 = [2*x | x <- [1..10]]
+
+l2 = [2*x | x <- [1..20], (mod x 2) == 0]
+
+l3 = [2*x | x <- [1..20], (mod x 2) == 0, x>= 10]
